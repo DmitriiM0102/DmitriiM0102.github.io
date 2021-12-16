eea0a88916455380c2a1f49ce6226f4686079415
DmitriiM0102 <91017076+DmitriiM0102@users.noreply.github.com>
2021-12-16 19:45:10 +0300

    Add files via upload

--- 5/market.js
+++ 5/market.js
@@ -1,34 +1,29 @@
 ﻿document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("DOM fully loaded and parsed");
-});
-function isInt(a){
-  return /^\d+$/.test(a);
-}
-function isNumeric(a){
-  if(typeof a=="number"){
-    return !isNaN(a) && !isNaN(parseFloat(a));
-  }
-  return false;
+  console.log("DOM fully loaded and parsed")
+})
+function isNumber(a){ // проверка для количества (т.е кг)
+  if (typeof a === 'number' && a>0)
+      return !isNaN(a)
+    else return false
 }
 function summa() {
-  let price = document.getElementsByName("price");
-  let colvo = document.getElementsByName("amount");
-  let sum= parseFloat(price[0].value) * parseFloat(colvo[0].value);
-  let a1= parseFloat(price[0].value),a2=parseFloat(colvo[0].value);
-  if(!isNumeric(a1) || a1<=0){
-    alert("Укажите цену правильно");
-    return false;
+  let price = document.getElementById("price").value
+  let colvo = document.getElementById("amount").value
+  let sum= +price * +colvo
+  let a1= +price, a2= parseFloat(colvo)
+  if(!isNumber(a1)){
+    alert("Укажите цену правильно")
+    return false
   }
-  if(!isInt(a2)){
-    alert("Укажите кол-во товаров правильно");
-    return false;
+  if(!isNumber(a2)){
+    alert("Укажите кол-во товаров правильно")
+    return false
   }
-  document.getElementById("result").innerHTML = parseFloat(price[0].value) * parseFloat(colvo[0].value);
   if (sum%10==1 && sum%100!=11)
-    document.getElementById("result").innerHTML = "Сумма: "+sum+" рубль";
+    document.getElementById("result").innerHTML = "Сумма: "+sum+" рубль"
   else
     if(sum%10<5 && sum%10!=0 && (sum%100<10 || sum%100>20))
-      document.getElementById("result").innerHTML = "Сумма: "+sum+" рубля";
-    else document.getElementById("result").innerHTML = "Сумма: "+sum+" рублей";
-  return false;
+      document.getElementById("result").innerHTML = "Сумма: "+sum+" рубля"
+    else document.getElementById("result").innerHTML = "Сумма: "+sum+" рублей"
+  return false
 }
